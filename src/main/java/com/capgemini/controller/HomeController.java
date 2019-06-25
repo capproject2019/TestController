ffce827f27fe11850fc3bab21393c7100fe4a1e6
Tokarski <jason.tokarski@capgemini.com>
2019-06-25 10:04:32 -0500

    Initial Commit

---
+++ src/main/java/com/capgemini/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.capgemini.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.capgemini.model.AccountRequest;
+import com.capgemini.model.AccountResponse;
+
+@RestController
+public class HomeController {
+
+	@RequestMapping(value = "/getAccount", method = RequestMethod.POST)
+	public ResponseEntity<?> GetAccountInformation(@RequestBody AccountRequest accountRequest) {
+		return ResponseEntity.ok(new AccountResponse("successful"));
+	}
+}
---
+++ src/main/java/com/capgemini/model/AccountResponse.java
@@ -0,0 +1,23 @@
+package com.capgemini.model;
+
+public class AccountResponse {
+	private String status;
+
+	public AccountResponse() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public AccountResponse(String status) {
+		super();
+		this.status = status;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
